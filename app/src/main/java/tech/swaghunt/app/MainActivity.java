58d3382c8c207f97b7f6da1b492fe97b7fc64598
smudigere <samarthamudigere@hotmail.com>
2018-09-22 14:44:38 -0400

    Play and Create buttons added in MainActivity.

--- app/src/main/java/tech/swaghunt/app/MainActivity.java
+++ app/src/main/java/tech/swaghunt/app/MainActivity.java
@@ -2,12 +2,24 @@ package tech.swaghunt.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mPlay, mCreate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init_ui();
+    }
+
+
+    private void init_ui()  {
+
+        mPlay = findViewById(R.id.play_button);
+        mCreate = findViewById(R.id.create_button);
     }
 }
