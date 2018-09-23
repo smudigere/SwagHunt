eeff319712f6105787ec2314aa5dcacd095e559f
Pankaj <mepankaj@live.in>
2018-09-22 22:43:00 -0400

    create hunt method added

--- app/src/main/java/tech/swaghunt/app/MainActivity.java
+++ app/src/main/java/tech/swaghunt/app/MainActivity.java
@@ -1,10 +1,12 @@
 package tech.swaghunt.app;
 
 import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.MimeTypeMap;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -16,12 +18,13 @@ import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import tech.swaghunt.app.models.Hunt;
 import tech.swaghunt.app.models.Image;
 import tech.swaghunt.app.models.Player;
+import tech.swaghunt.app.models.Task;
 
 public class MainActivity extends AppCompatActivity {
     private DatabaseReference mDatabase;
@@ -37,33 +40,39 @@ public class MainActivity extends AppCompatActivity {
         FirebaseStorage storage = FirebaseStorage.getInstance();
         storageReference = storage.getReference();
 
-        List<Image> images = new ArrayList<>();
-        images.add(new Image("img1Ref", "url"));
-        images.add(new Image("img2Ref", "url"));
-
-        List<String> texts = new ArrayList<>();
-        texts.add("txt1");
-        texts.add("txt2");
-
-        List<String> qrCodes = new ArrayList<>();
-        qrCodes.add("qrCode1");
-        qrCodes.add("qrCode2");
-
-        List<String> tasksIDsCompleted = new ArrayList<>();
-        tasksIDsCompleted.add("taskId1");
-        tasksIDsCompleted.add("taskID2");
-
-        writeNewPlayer("playerName1", "huntIDBeingPlayed", false,
-                images, texts, qrCodes, tasksIDsCompleted);
+//        List<Image> images = new ArrayList<>();
+//        images.add(new Image("img1Ref", "url"));
+//        images.add(new Image("img2Ref", "url"));
+//
+//        List<String> texts = new ArrayList<>();
+//        texts.add("txt1");
+//        texts.add("txt2");
+//
+//        List<String> qrCodes = new ArrayList<>();
+//        qrCodes.add("qrCode1");
+//        qrCodes.add("qrCode2");
+//
+//        List<String> tasksIDsCompleted = new ArrayList<>();
+//        tasksIDsCompleted.add("taskId1");
+//        tasksIDsCompleted.add("taskID2");
+//
+//        writeNewPlayer("playerName1", "huntIDBeingPlayed", false,
+//                images, texts, qrCodes, tasksIDsCompleted);
 
     }
 
-    private void writeNewPlayer(String name, String huntPlaying,
-                                Boolean hasWon, List<Image> images, List<String> text,
+    private void createPlayer(String name, String huntPlaying,
+                              Boolean hasWon, List<Image> images, List<String> text,
                                 List<String> qrCodes, List<String> tasksIDsCompleted) {
         Player player = new Player(name, huntPlaying, hasWon, images, text, qrCodes, tasksIDsCompleted);
         String playerID = mDatabase.push().getKey();
         mDatabase.child("player").child(playerID).setValue(player);
+    }
+
+    private void createHunt(String name, List<Task> tasks, String qrCode) {
+        Hunt hunt = new Hunt(name, tasks, qrCode);
+        String huntID = mDatabase.push().getKey();
+        mDatabase.child("hunt").child(huntID).setValue(hunt);
     }
 
     private void uploadImage(Uri filePath) {
@@ -104,5 +113,10 @@ public class MainActivity extends AppCompatActivity {
                         }
                     });
         }
+    }
+    public String getFileExtension(Uri uri) {
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
     }
 }
