b8a3b3d2d7d4c7534acf85ad5339b8eb4dff0b5b
Samartha Mudigere <smudigere@couponwallet.com>
2018-09-23 10:22:16 -0400

    PlayFragment ui.

--- app/src/main/java/tech/swaghunt/app/fragments/PlayFragment.java
+++ app/src/main/java/tech/swaghunt/app/fragments/PlayFragment.java
@@ -7,11 +7,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import tech.swaghunt.app.R;
+import tech.swaghunt.app.utility.Functions;
 
-public class PlayFragment extends Fragment {
+public class PlayFragment extends Fragment implements
+        View.OnClickListener{
 
+    private Button mCameraButton, mSubmitButton;
+    private TextView mTaskCountTextView, mClueText;
 
     @Nullable
     @Override
@@ -25,5 +31,39 @@ public class PlayFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
 
+        init_button(view);
+        init_text(view);
+    }
+
+
+    private void init_text(View view)    {
+
+        mTaskCountTextView = view.findViewById(R.id.task_count_text);
+        mClueText = view.findViewById(R.id.clue_text);
+    }
+
+
+    private void init_button(View view) {
+        mCameraButton = view.findViewById(R.id.scan_image_button);
+        mSubmitButton = view.findViewById(R.id.submit_button);
+
+        mCameraButton.setOnClickListener(this);
+        mSubmitButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId())   {
+
+            case R.id.scan_image_button:
+
+
+
+                break;
+            case R.id.submit_button:
+                Functions.popBackStack(getFragmentManager());
+                break;
+        }
     }
 }
